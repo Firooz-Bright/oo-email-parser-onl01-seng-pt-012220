e3ca6086829c5e412cd14b4f729255a4c5908a2e
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-02-08 23:38:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -21,6 +21,6 @@ class EmailParser
 end  
 
 email_addresses = "john@doe.com, person@somewhere.org"
-parser = EmailAddressParser.new(email_addresses)
+parser = EmailParser.new(email_addresses)
  
 parser.parse
