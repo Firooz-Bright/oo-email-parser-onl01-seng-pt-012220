d14306ebc1f08ea05cc63fdd01ffaca7aca9cbaf
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-02-08 23:27:18 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,7 +4,20 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
-   
+   attr_accessor  :csv_emails
+  
+  def initialize(csv_emails)
+
+    @csv_emails = csv_emails
+  end 
+
+
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
    
    
    
